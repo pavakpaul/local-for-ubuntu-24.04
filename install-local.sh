5c4c2cb61512ba83ffb2fc89032aa4bed2d0aa36
Pavak Paul <pavakatubuntu@gmail.com>
2024-11-18 15:49:53 +0530

    planning

---
+++ install-local.sh
@@ -0,0 +1,15 @@
+# Verify Ubuntu 24.04
+
+# Extract deb
+
+# Edit dependancy requirements
+
+# Build deb
+
+# Install deb
+#sudo apt install ./local.deb
+
+# Create symbolic links
+
+# Fix permissions
+
