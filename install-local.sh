f520057bf3981ef1c8c7121e1a1be7351abe5e47
Pavak Paul <pavakatubuntu@gmail.com>
2024-11-19 11:50:42 +0530

    added -y to apt

--- install-local.sh
+++ install-local.sh
@@ -10,7 +10,7 @@ wget https://cdn.localwp.com/stable/latest/deb -O local-orig.deb
 echo "Replacing dependancies..."
 fakeroot bash -c 'dpkg-deb -R local-orig.deb workingdir; sed -i 's/libaio1/libaio1t64/g' workingdir/DEBIAN/control; sed -i 's/libncurses5/libncurses6/g' workingdir/DEBIAN/control; dpkg-deb -b workingdir local-fixed.deb'
 echo "Installing Local..."
-pkexec apt install $TMPDIR/local-fixed.deb
+pkexec apt install -y $TMPDIR/local-fixed.deb
 echo "Removing temporary files..."
 rm -rf $TMPDIR
 
