5d79c184f0148b682df9b83d43169df19bb572ef
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2024-11-19 10:39:27 +0530

    Update install-local.sh

--- install-local.sh
+++ install-local.sh
@@ -1,15 +1,10 @@
-# Verify Ubuntu 24.04
+#!/bin/bash
 
-# Extract deb
+grep VERSION_ID /etc/os-release | grep 24.04 || exit
 
-# Edit dependancy requirements
-
-# Build deb
-
-# Install deb
-#sudo apt install ./local.deb
-
-# Create symbolic links
-
-# Fix permissions
-
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
+wget https://cdn.localwp.com/stable/latest/deb -O local-orig.deb
+fakeroot bash -c 'dpkg-deb -R local-orig.deb workingdir; sed -i 's/libaio1/libaio1t64/g' workingdir/DEBIAN/control; sed -i 's/libncurses5/libncurses6/g' workingdir/DEBIAN/control; dpkg-deb -b workingdir local-fixed.deb'
+pkexec apt install $TMPDIR/local-fixed.deb
+rm -rf $TMPDIR
